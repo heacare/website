8386d0b08353a768386609399a0872b0eb0a2bc4
Ambrose Chua <ambrose@hea.care>
2022-09-01 17:59:44 +0800

    Update links

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const discordLink = "https://discord.gg/MsZVV6Z3sU";
+export const twitterLink = "https://twitter.com/hea_care";
+export const instagramLink = "https://www.instagram.com/hea_care";
+export const githubLink = "https://github.com/heacare";
+export const blogLink = "https://mirror.xyz/blog.heacare.eth";
+export const manifestoLink = "https://mirror.xyz/blog.heacare.eth";
