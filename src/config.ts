47f29179d06346b3f5d8325960dc83887b21da2b
Ambrose Chua <ambrose@hea.care>
2022-09-02 13:51:58 +0800

    Add Twitter and Discord buttons to navigation

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const twitterLink = "https://twitter.com/hea_care";
 export const instagramLink = "https://www.instagram.com/hea_care";
 export const githubLink = "https://github.com/heacare";
 export const blogLink = "https://mirror.xyz/blog.heacare.eth";
-export const manifestoLink = "https://mirror.xyz/blog.heacare.eth";
+export const manifestoLink = "https://mirror.xyz/blog.heacare.eth/4zJjwIysK6jJKCQdAiou9kHB_GuZ8W0gHMAouZVB2uE";
