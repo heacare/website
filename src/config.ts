f10a82eb17982bad4709d7f3a3f5ebe45fdb53b2
Sudharshan <sudharshan@hey.com>
2022-09-04 11:33:24 +0800

    Added proper newsletter link

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const instagramLink = "https://www.instagram.com/hea_care";
 export const githubLink = "https://github.com/heacare";
 export const blogLink = "https://mirror.xyz/blog.heacare.eth";
 export const wikiLink = "https://heacare.notion.site";
-export const newsletterLink = "https://heacare.notion.site";
+export const newsletterLink = "https://heacare.substack.com";
 export const manifestoLink = "https://mirror.xyz/blog.heacare.eth/4zJjwIysK6jJKCQdAiou9kHB_GuZ8W0gHMAouZVB2uE";
