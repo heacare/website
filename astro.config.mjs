5915808eba9aeb7d94d84f05b5550282a80d58ae
Ambrose Chua <ambrose@hea.care>
2022-10-07 12:08:32 +0800

    Format code, add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    ssr: {
+      // Importing Chakra UI with the default settings currently fails. To
+      // work around this, we can either import the modules as CommonJS:
+      format: "cjs",
+      // Or we can concatenate all the dependencies into the file used for
+      // SSR (except for React, for an obscure reason):
+      //noExternal: true,
+      //external: ['react'],
+      // However, the latter breaks the development server.
+    },
+  },
+});
