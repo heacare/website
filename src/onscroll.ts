d5c6d33ced3fa1da21810ae11b17b383a808a33d
Ambrose Chua <ambrose@hea.care>
2022-12-09 17:12:35 +0800

    Fix roadmap and add parallax scroll

---
+++ src/onscroll.ts
@@ -0,0 +1,51 @@
+type ScrollData = {
+  scrollX: number;
+  scrollY: number;
+  innerWidth: number;
+  innerHeight: number;
+};
+
+type ScrollHandler = (data: ScrollData) => void;
+
+let data = <ScrollData>{
+  scrollX: 0,
+  scrollY: 0,
+  innerWidth: 0,
+  innerHeight: 0,
+};
+let ticking = false;
+let handlers = <ScrollHandler[]>[];
+
+document.addEventListener("scroll", (e) => {
+  data = {
+    scrollX: window.scrollX,
+    scrollY: window.scrollY,
+    innerWidth: window.innerWidth,
+    innerHeight: window.innerHeight,
+  };
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      for (const handler of handlers) {
+        handler(data);
+      }
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
+
+export default function onScroll<K, T>(
+  elements: NodeListOf<K>,
+  initInfos: (element: Node) => T,
+  handler: (info: T, data: ScrollData) => void
+) {
+  const infos = <T[]>[];
+  elements.forEach((element) => {
+    infos.push(initInfos(element));
+  });
+  handlers.push((data: ScrollData) => {
+    for (const info of infos) {
+      handler(info, data);
+    }
+  });
+}
